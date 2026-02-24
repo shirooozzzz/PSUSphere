d2ef6a914a5ef5c8670478ce36f03470aec0d0eb
Denzel Andrei Bermejo <shirooozzz13@gmail.com>
2026-02-24 19:34:00 +0800

    updated models.py

--- studentorg/models.py
+++ studentorg/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 
-# Base model to inherit common fields
+
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        abstract = True  # So Django doesn't create a table for BaseModel
+        abstract = True
 
-# College model
+
 class College(BaseModel):
     college_name = models.CharField(max_length=150)
 
     def __str__(self):
         return self.college_name
 
-# Program model
+
 class Program(BaseModel):
     prog_name = models.CharField(max_length=150)
     college = models.ForeignKey(College, on_delete=models.CASCADE)
@@ -23,7 +23,7 @@ class Program(BaseModel):
     def __str__(self):
         return self.prog_name
 
-# Organization model
+
 class Organization(BaseModel):
     name = models.CharField(max_length=250)
     college = models.ForeignKey(College, null=True, blank=True, on_delete=models.CASCADE)
@@ -32,7 +32,7 @@ class Organization(BaseModel):
     def __str__(self):
         return self.name
 
-# Student model
+
 class Student(BaseModel):
     student_id = models.CharField(max_length=15)
     lastname = models.CharField(max_length=25)
@@ -43,7 +43,7 @@ class Student(BaseModel):
     def __str__(self):
         return f"{self.lastname}, {self.firstname}"
 
-# OrgMember model
+
 class OrgMember(BaseModel):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
