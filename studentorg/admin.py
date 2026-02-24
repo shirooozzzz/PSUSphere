e95c72bc4d69f740fe7e8da094003d95884a55eb
Denzel Andrei Bermejo <shirooozzz13@gmail.com>
2026-02-24 18:37:35 +0800

    change admin.py to normal

--- studentorg/admin.py
+++ studentorg/admin.py
@@ -1,57 +1,27 @@
 from django.contrib import admin
 from .models import College, Program, Organization, Student, OrgMember
 
-
-# -------------------------
-# College Admin
-# -------------------------
-@admin.register(College)
-class CollegeAdmin(admin.ModelAdmin):
-    list_display = ("college_name", "created_at", "updated_at")
-    search_fields = ("college_name",)
-    list_filter = ("created_at",)
+admin.site.register(College)
+admin.site.register(Program)
+admin.site.register(Organization)
 
 
-# -------------------------
-# Program Admin
-# -------------------------
-@admin.register(Program)
-class ProgramAdmin(admin.ModelAdmin):
-    list_display = ("prog_name", "college", "created_at")
-    search_fields = ("prog_name", "college__college_name")
-    list_filter = ("college",)
-
-
-# -------------------------
-# Organization Admin
-# -------------------------
-@admin.register(Organization)
-class OrganizationAdmin(admin.ModelAdmin):
-    list_display = ("name", "college", "description", "created_at")
-    search_fields = ("name", "description")
-    list_filter = ("college",)
-
-
-# -------------------------
-# Student Admin
-# -------------------------
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ("student_id", "lastname", "firstname", "middlename", "program")
-    search_fields = ("lastname", "firstname")
-    list_filter = ("program",)
+    search_fields = ("lastname", "firstname",)
 
 
-# -------------------------
-# OrgMember Admin
-# -------------------------
 @admin.register(OrgMember)
 class OrgMemberAdmin(admin.ModelAdmin):
-    list_display = ("student", "get_member_program", "organization", "date_joined")
-    search_fields = ("student__lastname", "student__firstname")
-    list_filter = ("organization",)
+    list_display = ("student", "get_member_program", "organization", "date_joined",)
+    search_fields = ("student__lastname", "student__firstname",)
 
     def get_member_program(self, obj):
-        return obj.student.program
+        try:
+            member = Student.objects.get(id=obj.student_id)
+            return member.program
+        except Student.DoesNotExist:
+            return None
 
     get_member_program.short_description = "Program"
