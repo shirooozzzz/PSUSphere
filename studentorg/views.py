601c5e09d30edd80d39b015b8788599885b9d054
Denzel Andrei Bermejo <shirooozzz13@gmail.com>
2026-02-25 18:58:22 +0800

    added org_list.html

--- studentorg/views.py
+++ studentorg/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from studentorg.models import Organization
 
+
 class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
+
+
+class OrganizationList(ListView):
+    model = Organization
+#    context_object_name = 'organization'
+    template_name = 'org_list.html'
+    paginate_by = 5
