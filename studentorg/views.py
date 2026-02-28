bf81ab5dc95c23039cbf77b822ad050cce254390
Denzel Andrei Bermejo <shirooozzz13@gmail.com>
2026-02-28 23:51:32 +0800

    show number of students

--- studentorg/views.py
+++ studentorg/views.py
@@ -13,12 +13,30 @@ from studentorg.models import OrgMember
 from studentorg.forms import OrgMemberForm
 from django.urls import reverse_lazy
 from django.db.models import Q
+from django.utils import timezone
 
 
 class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["total_students"] = Student.objects.count()
+
+        today = timezone.now().date()
+        count = (
+            OrgMember.objects.filter(
+                date_joined__year=today.year
+            )
+            .values("student")
+            .distinct()
+            .count()
+        )
+
+        context["students_joined_this_year"] = count
+        return context
 
 
 class OrganizationList(ListView):
