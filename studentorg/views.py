79f8d995f8352e60da39b1306b3f069c1abafd82
Denzel Andrei Bermejo <shirooozzz13@gmail.com>
2026-03-01 11:14:42 +0800

    add org and prog in dashboard

--- studentorg/views.py
+++ studentorg/views.py
@@ -24,6 +24,8 @@ class HomePageView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["total_students"] = Student.objects.count()
+        context["total_organizations"] = Organization.objects.count()
+        context["total_programs"] = Program.objects.count()
 
         today = timezone.now().date()
         count = (
