1d6d0bacd83ed3c89ea27b28c1778b4e1c234556
Denzel Andrei Bermejo <shirooozzz13@gmail.com>
2026-03-06 10:56:21 +0800

    added auth login

--- projectsite/settings.py
+++ projectsite/settings.py
@@ -42,8 +42,25 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.github',
+
     "studentorg",
     "widget_tweaks",
+
+]
+
+SITE_ID = 5
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 MIDDLEWARE = [
@@ -52,6 +69,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -117,6 +135,23 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+]
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/accounts/login/'            # where @login_required redirects
+LOGIN_REDIRECT_URL = '/'                  # after successful login
+LOGOUT_REDIRECT_URL = '/accounts/login/'  # after logout
+ACCOUNT_LOGOUT_REDIRECT_URL = '/'          # allauth logout redirect
+ACCOUNT_LOGOUT_ON_GET = True               # logout via GET request
+
+ACCOUNT_LOGIN_METHODS = {"username", "email"}  # login using username OR email
+
+ACCOUNT_SIGNUP_FIELDS = [
+    "username*",
+    "email*",
+    "password1*",
+    "password2*",
 ]
 
 
